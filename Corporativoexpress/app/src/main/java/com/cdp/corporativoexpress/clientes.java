27617bdfd811b73820762373038771267246ac84
carlos-e21 <carloseduardomunoz98@gmail.com>
2023-08-01 00:50:56 -0500

    ya logre agregar clientes a la base de datos

--- Corporativoexpress/app/src/main/java/com/cdp/corporativoexpress/clientes.java
+++ Corporativoexpress/app/src/main/java/com/cdp/corporativoexpress/clientes.java
@@ -1,5 +1,6 @@
 package com.cdp.corporativoexpress;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,8 +10,11 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -29,38 +33,86 @@ public class clientes extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clientes);
 
-        edtnombre =(EditText)findViewById(R.id.edtnombre);
-        edtapellido = (EditText)findViewById(R.id.edtapellido);
-        edtdireccion =(EditText)findViewById(R.id.edtdireccion);
-        edtcorreo =(EditText)findViewById(R.id.edtcorreo);
-        edttelefono = (EditText)findViewById(R.id.edttelefono);
-        btguardarclientes = (Button)findViewById(R.id.btguardarclientes);
+        edtnombre =findViewById(R.id.edtnombre);
+        edtapellido = findViewById(R.id.edtapellido);
+        edtdireccion =findViewById(R.id.edtdireccion);
+        edtcorreo =findViewById(R.id.edtcorreo);
+        edttelefono =findViewById(R.id.edttelefono);
+        btguardarclientes =findViewById(R.id.btguardarclientes);
 
-        btguardarclientes.setOnClickListener(v -> ejecutarclientes());
+        btguardarclientes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ejecutarclientes();
+            }
+        });
 
 
     }
 
     private void ejecutarclientes() {
-        StringRequest stringRequest = new StringRequest(
-                Request.Method.POST, "http://192.168.100.64:80/programador/clientes.php",
-                response -> Toast.makeText(getApplicationContext(), "SE GUARDO EL CLIENTE CORRECTAMENTE", Toast.LENGTH_SHORT).show(),
-                error -> Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show()) {
 
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> parametros = new HashMap<>();
-                parametros.put("nombre", edtnombre.getText().toString());
-                parametros.put("apellido", edtapellido.getText().toString());
-                parametros.put("direccion", edtdireccion.getText().toString());
-                parametros.put("correo", edtcorreo.getText().toString());
-                parametros.put("telefono", edttelefono.getText().toString());
-                return parametros;
+    String nombre= edtnombre.getText().toString().trim();
+    String apellidos= edtapellido.getText().toString().trim();
+    String direccion = edtdireccion.getText().toString().trim();
+    String email=edtcorreo.getText().toString().trim();
+    String telefono= edttelefono.getText().toString().trim();
+
+        ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("cargando");
+
+        if (nombre.isEmpty()){
+            Toast.makeText(this,"ingrese nombre",Toast.LENGTH_SHORT).show();
+        } else if (apellidos.isEmpty()) {
+            Toast.makeText(this,"ingrese apellidos",Toast.LENGTH_SHORT).show();
+        } else if (direccion.isEmpty()) {
+            Toast.makeText(this,"ingrese direccion",Toast.LENGTH_SHORT).show();
+        } else if (email.isEmpty()) {
+            Toast.makeText(this,"ingrese email",Toast.LENGTH_SHORT).show();
+        }
+        else if (telefono.isEmpty()) {
+           Toast.makeText(this,"ingrese numero",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            progressDialog.show();
+            StringRequest request = new StringRequest(Request.Method.POST, "http://192.168.100.64/programador/clientes.php",
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            if (response.equalsIgnoreCase("datos insertados")) {
+                                Toast.makeText(clientes.this, "registrado correctamente", Toast.LENGTH_SHORT).show();
+                                progressDialog.dismiss();
+                                startActivity(new Intent(getApplicationContext(), clientes.class));
+                                finish();
+                            } else {
+                                Toast.makeText(clientes.this, "ERROR, No se puede registrar", Toast.LENGTH_SHORT).show();
+                                progressDialog.dismiss();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(clientes.this,error.getMessage(),Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+                }
             }
-        };
+            ){
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> parametros = new HashMap<>();
+                    parametros.put("nombre", nombre);
+                    parametros.put("apellido", apellidos);
+                    parametros.put("direccion", direccion);
+                    parametros.put("correo", email);
+                    parametros.put("telefono", telefono);
+                    return parametros;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(clientes.this);
+            requestQueue.add(request);
+        }
 
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(stringRequest);
+
     }
 
     //botones
