7da2c2583418e619e8bbe3cc2df5cb3dac021c0a
carlos-e21 <carloseduardomunoz98@gmail.com>
2023-08-15 15:21:57 -0600

    listo

--- Corporativoexpress/app/src/main/java/com/cdp/corporativoexpress/acercade.java
+++ Corporativoexpress/app/src/main/java/com/cdp/corporativoexpress/acercade.java
@@ -1,28 +1,51 @@
 package com.cdp.corporativoexpress;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class acercade extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acercade);
 
+    Button btnoficina = findViewById(R.id.btnoficina);
+        Button btnsyscom = findViewById(R.id.btnsyscom);
 
+        btnoficina.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String phoneoficina="tel:9611321079";
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse(phoneoficina));
+                startActivity(intent);
+
+            }
+        });
+
+        btnsyscom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String phonesyscom="tel:6144152525";
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse(phonesyscom));
+                startActivity(intent);
+
+            }
+        });
 
     }
+
+
 
 
     public void regresar(View view)
